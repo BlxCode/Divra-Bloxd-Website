2199fc74c59667bee34a9d8c601355cb29e3d0ed
BlxCode <148653492+BlxCode@users.noreply.github.com>
2024-05-18 23:47:10 -0700

    asdf

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ console.log(screen.width);
 console.log(screen.availWidth);
 if(screen.availWidth<540){
   document.getElementById("MobileNavBar").hidden=false;
-  document.getElementById("TOP").hidden=true;
+  document.getElementById("TOP").remove();
 }else if(screen.availWidth>539){
   document.getElementById("MobileNavBar").remove();
   document.getElementById("TOP").hidden=false;
